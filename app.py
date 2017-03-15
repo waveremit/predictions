144268cdcb023daf2e6091c79b9ce38bd347378f
Jeff Kaufman <jeff@jefftk.com>
2017-03-14 22:01:14 -0500

    accept posts

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['POST'])
+def main():
+    return '\n'.join('%s: %s' % (k,v)for (k,v) in request.form.items())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
      app.debug = True
      port = int(os.environ.get("PORT", 5000))
      app.run(host='0.0.0.0', port=port)
