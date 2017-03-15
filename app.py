70d6ac87d6e3d6f58cdffe0533330c2dd0c47109
Jeff Kaufman <jeff@jefftk.com>
2017-03-14 21:38:54 -0500

    try deploying to heroku

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
