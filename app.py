396cfa7f127187b18aa6a9f23be33fa251ec5ca0
Jeff Kaufman <jeff@jefftk.com>
2017-03-14 22:08:55 -0500

    echo

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os
+import json
+
 from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def main():
-    return '\n'.join('%s: %s' % (k,v)for (k,v) in request.form.items())
+    if request.form['token'] != os.environ('SLACK_TOKEN'):
+        raise Exception('invalid token')
+    response = dict(response_type='ephemeral',
+                    text=request.form['text'])
+    return json.dumps(response)
 
 if __name__ == '__main__':
      app.debug = True
